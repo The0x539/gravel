c89bba185486c0f649f65c9824d33cd23151de20
The0x539 <the0x539@gmail.com>
2026-06-23 16:42:40 -0500

    refactor safe bindings for touch module

--- gravel/src/foundation/event/touch.rs
+++ gravel/src/foundation/event/touch.rs
@@ -1,55 +1,59 @@
 use alloc::boxed::Box;
+use core::cell::UnsafeCell;
 use core::ffi::c_void;
 use gravel_sys::foundation::event::touch as sys;
 pub use sys::TouchEvent;
 
-// It's kinda nuts that I needed to implement this using a global variable.
-static mut CLEANUP_DATA: Option<(*mut c_void, unsafe fn(*mut c_void))> = None;
+pub trait TouchServiceHandler: FnMut(&TouchEvent) + 'static {}
+impl<F: FnMut(&TouchEvent) + 'static> TouchServiceHandler for F {}
 
-fn cleanup() {
-    unsafe {
-        let Some((data, func)) = CLEANUP_DATA else {
-            return;
-        };
-        CLEANUP_DATA = None;
-        func(data);
-    }
-}
-
-unsafe fn cleanup_func<H: TouchServiceHandler>(data: *mut c_void) {
-    unsafe {
-        let data: *mut H = data.cast();
-        drop(Box::from_raw(data));
+extern "C" fn sys_handler(event: *const TouchEvent, _: *mut c_void) {
+    if let Some(f) = GLOBAL_HANDLER.get_ref() {
+        unsafe { f(&*event) }
     }
 }
 
 pub fn subscribe<H: TouchServiceHandler>(handler: H) {
-    cleanup();
-
-    let data = Box::into_raw(Box::new(handler)).cast::<c_void>();
-    unsafe {
-        CLEANUP_DATA = Some((data, cleanup_func::<H>));
-        sys::touch_service_subscribe(H::handle_sys, data);
+    if GLOBAL_HANDLER.replace(Box::new(handler)).is_none() {
+        unsafe { sys::touch_service_subscribe(sys_handler, core::ptr::null_mut()) }
     }
 }
 
 pub fn unsubscribe() {
-    cleanup();
-    unsafe { sys::touch_service_unsubscribe() }
+    if GLOBAL_HANDLER.take().is_some() {
+        unsafe { sys::touch_service_unsubscribe() }
+    }
 }
 
 pub fn is_enabled() -> bool {
     unsafe { sys::touch_service_is_enabled() }
 }
 
-pub trait TouchServiceHandler: FnMut(&TouchEvent) + Sized + 'static {
-    unsafe extern "C" fn handle_sys(event: *const TouchEvent, context: *mut c_void) {
-        unsafe {
-            let this = &mut *context.cast::<Self>();
-            let event = &*event;
-            this(event);
-        }
+// It's kinda nuts that I needed to implement this using a global variable.
+// This is necessary in order to run the destructor of a previous subscription.
+// As a side effect of this, the context parameter isn't used for this module,
+// because we can store everything we need inside the global variable.
+static GLOBAL_HANDLER: GlobalCell<Box<dyn TouchServiceHandler>> = GlobalCell::new();
+
+struct GlobalCell<T>(UnsafeCell<Option<T>>);
+
+/// SAFETY: This platform does not have threads.
+unsafe impl<T> Sync for GlobalCell<T> {}
+
+impl<T> GlobalCell<T> {
+    const fn new() -> Self {
+        Self(UnsafeCell::new(None))
+    }
+
+    fn get_ref(&self) -> &mut Option<T> {
+        unsafe { &mut *self.0.get() }
+    }
+
+    fn take(&self) -> Option<T> {
+        self.get_ref().take()
+    }
+
+    fn replace(&self, value: T) -> Option<T> {
+        self.get_ref().replace(value)
     }
 }
-
-impl<F: FnMut(&TouchEvent) + 'static> TouchServiceHandler for F {}
